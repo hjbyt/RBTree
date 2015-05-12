85b655d38df19b372681362d98cc0d0cf6207ffc
hjbyt <sopu00@gmail.com>
2015-05-12 12:57:36 +0300

    added tree printing functionality

--- RBTree/src/RBTree.java
+++ RBTree/src/RBTree.java
@@ -1,4 +1,6 @@
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.function.BiConsumer;
@@ -49,6 +51,7 @@ public class RBTree {
 
         nil = new RBNode(Color.Black);
         nil.parent = rootDummy;
+        //TODO: shuold nil's children be null?
         nil.left = nil;
         nil.right = nil;
         rootDummy.left = nil;
@@ -535,8 +538,7 @@ public class RBTree {
     }
 
     private void printTree(PrintStream stream) {
-        //TODO: print tree graph to console, to help debug problems in insert, delete.
-        stream.println("---TODO---");
+        TreePrinter.print(this.rootDummy, stream);
     }
 
     // non-private for testing purposes
@@ -628,7 +630,7 @@ public class RBTree {
         }
     }
 
-    private class RBNode {
+    private class RBNode implements TreePrinter.PrintableNode {
 
         public RBNode parent;
         public RBNode left;
@@ -724,6 +726,172 @@ public class RBTree {
         public boolean hasRightChild() {
             return right != nil;
         }
+
+        public TreePrinter.PrintableNode getLeft() {
+            if (this == nil) {
+                return null;
+            } else {
+                return left;
+            }
+        }
+
+        public TreePrinter.PrintableNode getRight() {
+            if (this == nil) {
+                return null;
+            } else {
+                return right;
+            }
+        }
+
+        public String getText() {
+            String color_string = (color == Color.Black) ? "B" : "R";
+            return String.format("%s-%d:%s", color_string, key, item);
+        }
     }
 
+}
+
+
+//adapted from http://stackoverflow.com/a/29704252
+/**
+ * Binary tree printer
+ *
+ * @author MightyPork
+ */
+class TreePrinter {
+    /**
+     * Node that can be printed
+     */
+    public interface PrintableNode {
+        /**
+         * Get left child
+         */
+        PrintableNode getLeft();
+
+
+        /**
+         * Get right child
+         */
+        PrintableNode getRight();
+
+
+        /**
+         * Get text to be printed
+         */
+        String getText();
+    }
+
+    /**
+     * Print a tree
+     *
+     * @param root tree root node
+     */
+    public static void print(PrintableNode root, PrintStream stream) {
+        List<List<String>> lines = new ArrayList<>();
+
+        List<PrintableNode> level = new ArrayList<>();
+        List<PrintableNode> next = new ArrayList<>();
+
+        level.add(root);
+        int nn = 1;
+
+        int widest = 0;
+
+        while (nn != 0) {
+            List<String> line = new ArrayList<>();
+
+            nn = 0;
+
+            for (PrintableNode n : level) {
+                if (n == null) {
+                    line.add(null);
+
+                    next.add(null);
+                    next.add(null);
+                } else {
+                    String aa = n.getText();
+                    line.add(aa);
+                    if (aa.length() > widest) widest = aa.length();
+
+                    next.add(n.getLeft());
+                    next.add(n.getRight());
+
+                    if (n.getLeft() != null) nn++;
+                    if (n.getRight() != null) nn++;
+                }
+            }
+
+            if (widest % 2 == 1) widest++;
+
+            lines.add(line);
+
+            List<PrintableNode> tmp = level;
+            level = next;
+            next = tmp;
+            next.clear();
+        }
+
+        int perpiece = lines.get(lines.size() - 1).size() * (widest + 4);
+        for (int i = 0; i < lines.size(); i++) {
+            List<String> line = lines.get(i);
+            int hpw = (int) Math.floor(perpiece / 2f) - 1;
+
+            if (i > 0) {
+                for (int j = 0; j < line.size(); j++) {
+
+                    // split node
+                    char c = ' ';
+                    if (j % 2 == 1) {
+                        if (line.get(j - 1) != null) {
+                            //c = (line.get(j) != null) ? '?' : '?';
+                            c = (line.get(j) != null) ? '.' : '.';
+                        } else {
+                            //if (j < line.size() && line.get(j) != null) c = '?';
+                            if (j < line.size() && line.get(j) != null) c = '.';
+                        }
+                    }
+                    stream.print(c);
+
+                    // lines and spaces
+                    if (line.get(j) == null) {
+                        for (int k = 0; k < perpiece - 1; k++) {
+                            stream.print(" ");
+                        }
+                    } else {
+
+                        for (int k = 0; k < hpw; k++) {
+                            //stream.print(j % 2 == 0 ? " " : "?");
+                            stream.print(j % 2 == 0 ? " " : ".");
+                        }
+                        //stream.print(j % 2 == 0 ? "?" : "?");
+                        stream.print(j % 2 == 0 ? "." : ".");
+                        for (int k = 0; k < hpw; k++) {
+                            //stream.print(j % 2 == 0 ? "?" : " ");
+                            stream.print(j % 2 == 0 ? "." : " ");
+                        }
+                    }
+                }
+                stream.println();
+            }
+
+            // print line of numbers
+            for (String f : line) {
+                if (f == null) f = "";
+                int gap1 = (int) Math.ceil(perpiece / 2f - f.length() / 2f);
+                int gap2 = (int) Math.floor(perpiece / 2f - f.length() / 2f);
+
+                // a number
+                for (int k = 0; k < gap1; k++) {
+                    stream.print(" ");
+                }
+                stream.print(f);
+                for (int k = 0; k < gap2; k++) {
+                    stream.print(" ");
+                }
+            }
+            stream.println();
+
+            perpiece /= 2;
+        }
+    }
 }
